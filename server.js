6a84990dbc1d820efc46aa5bc80fda1444cdddd9
zhane pryce <zhanefilms@gmail.com>
2021-04-18 11:37:17 +0100

    working rap api

---
+++ server.js
@@ -0,0 +1,47 @@
+const express = require('express')
+const app = express()
+const PORT = 8000
+const cors = require('cors')
+
+app.use(cors())
+
+let rappers ={
+'21 savage': {
+    'age': 28,
+    'birthName': 'Shéyaa Bin Abraham-Joseph',
+    'birthLocation': 'London, England'
+},
+'chance the rapper':{
+    'age': 27,
+    'birthName': 'Chancelor Jonathon Bennett',
+    'birthLocation': 'Chicago, Illinois'
+},
+'unknown':{
+    'age': 28,
+    'birthName': 'unknown',
+    'birthLocation': 'unknown'
+
+},
+}
+
+
+
+app.get('/', (request, response) => {
+    response.sendFile__dirname + './index.html'
+})
+
+api.get('/api/rappers/:rappersName', (request, response) => {
+const rapName = request.params.rapperName.toLowerCase()
+console.log(rapName)
+if(rappers[rapName]){
+response.json(rappers[rapName])
+}else {
+    response.json(rappers['unknown'])
+}
+
+})
+
+
+app.listen(PORT, () =>{
+    console.log(`sever running on port ${PORT}`)
+})
